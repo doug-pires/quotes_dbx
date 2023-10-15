a0fb53c912479cce1ace5b815f62fdd0f12e857d
doug-pires <dpiresmartins@yahoo.com.br>
2023-10-15 15:07:34 +0100

    test: Mock functions to test log errors and return values. Add function to save to dbutils

--- quotes_dbx/request_quote.py
+++ quotes_dbx/request_quote.py
@@ -1,4 +1,7 @@
+import json
 import os
+import random as rd
+from datetime import datetime
 
 import requests
 from dotenv import load_dotenv
@@ -15,12 +18,30 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY_NINJAS")
 
 
-def extract_quote():
-    category = "happiness"
+category_list = [
+    "age",
+    "alone",
+    "amazing",
+    "anger",
+    "architecture",
+    "art",
+    "attitude",
+    "beauty",
+    "best",
+    "birthday",
+]
+
+
+def pick_random_category(words_of_list: list) -> str:
+    return rd.choice(words_of_list)
+
+
+def extract_quote() -> list:
+    category = pick_random_category(category_list)
     api_url = f"https://api.api-ninjas.com/v1/quotes?category={category}"
     response = requests.get(api_url, headers={"X-Api-Key": API_KEY})
     if response.status_code == requests.codes.ok:
-        quote = response.text
+        quote = response.json()
         return quote
     else:
         logger.error(
@@ -28,8 +49,19 @@ def extract_quote():
         )
 
 
+def save_to_storage(path_dbfs: str, data: list) -> None:
+    json_formatted = json.dumps(data, indent=4, sort_keys=True)
+    json_datetime = f"{path_dbfs}data_json_{datetime.now().timestamp()}"
+    try:
+        dbutils.fs.put(json_datetime, json_formatted)
+        logger.info("Saved to %s", path_dbfs)
+    except AttributeError as e:
+        logger.error(e)
+
+
 def main():
-    extract_quote()
+    quote = extract_quote()
+    save_to_storage(path_dbfs="/mnt/json_dbx/", data=quote)
 
 
 if __name__ == "__main__":
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,12 +1,45 @@
-from quotes_dbx.request_quote import extract_quote
+from quotes_dbx.request_quote import extract_quote, pick_random_category
 
 
-def test_if_quote_return_string_when_success():
-    ...
+def test_return_one_string_randomly_between_list_of_categories():
+    # Give a list of countries
+
+    country_list = ["Germany", "Brazil", "Portugal", "India"]
+
+    # When I call my function to pick one randoly
+    random_country = pick_random_category(country_list)
+    # Then should pick one and this value should be at the list
+    assert random_country in country_list
+    assert isinstance(random_country, str)
+
+
+def test_if_quote_return_string_when_success(mocker):
     # Given the status code of sucess
+    mock_response = mocker.Mock()
+    mock_response.status_code = 200
+    # mock_response.text["This is a quote"]
+    mock_response.json.return_value = ["This is a quote"]
+    mocker.patch("requests.get", return_value=mock_response)
 
     # When we can the function and run succes
     quote = extract_quote()
 
     # Then the type should be json
-    assert isinstance(quote, str)
+    assert isinstance(quote, list)
+
+
+def test_log_error_when_function_return_400(mocker, caplog):
+    # Given the response returning 400
+    mock_response = mocker.Mock()
+    mock_response.status_code = 400
+    mock_response.text = "Bad Request"
+    mocker.patch("requests.get", return_value=mock_response)
+
+    # When we call the function will generate wrong status_code
+    extract_quote()
+
+    # Then the log Message should be checked
+    expected_log_message = (
+        f"Status Code: {mock_response.status_code} - Reason: {mock_response.text}"
+    )
+    assert expected_log_message in caplog.text
