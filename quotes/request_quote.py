d67118c6625a49dc79f855b3b1f218ecfddca85b
doug-pires <dpiresmartins@yahoo.com.br>
2023-10-30 23:01:05 +0000

    refactor: Mkdocs and tests

--- quotes/request_quote.py
+++ quotes/request_quote.py
@@ -136,6 +136,6 @@ def save_to_storage(
 def main():  # pragma: no cover
     w = authenticate_databricks()
     API_KEY = get_api_key(workspace=w)
-    quote = extract_quote(workspace=w, API_KEY=API_KEY)
+    quote = extract_quote(API_KEY=API_KEY)
     print(quote)
     save_to_storage(workspace=w, path_dbfs=path_landing_quotes_dbx, data=quote)
